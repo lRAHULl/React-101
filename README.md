4e36f0e28ce73d9e30cdefc3a917f9a871ee4535
llRAHULll <rahulreigns16@gmail.com>
2018-12-11 00:35:03 +0530

    changed jsx files to .jsx extension

--- README.md
+++ README.md
@@ -25,5 +25,5 @@ live-server public
 Turn the _babel-cli_ by typing the following _command_ in the terminal:
 
 ```bash
-babel src/app.js --out-file public/scripts/app.js --presets=env,react --watch
+babel src/app.jsx --out-file public/scripts/app.js --presets=env,react --watch
 ```
--- src/app.jsx
+++ src/app.jsx
@@ -1,68 +1,74 @@
 // JSX - javascript xml
-console.log("The App is running!");
+console.log('The App is running!');
 
 // if - statements
 // ternary operators
 // logical and operators
 
 const app = {
-  title: "React 101",
-  subTitle: "Let me take your Decision.",
-  options: []
+	title: 'React 101',
+	subTitle: 'Let me take your Decision.',
+	options: []
 };
 
-const onFormSubmit = (e) => {
-  e.preventDefault();
-  const option = e.target.elements.option.value;
-  if (option) {
-    app.options.push(option);
-    e.target.elements.option.value = "";
-    console.log(app.options);
-  }
-  renderTemplate();
+const onFormSubmit = e => {
+	e.preventDefault();
+	const option = e.target.elements.option.value;
+	if (option) {
+		app.options.push(option);
+		e.target.elements.option.value = '';
+		console.log(app.options);
+	}
+	renderTemplate();
 };
 
 const onRemoveAll = () => {
-  app.options = [];
-  console.log(app.options)
-  renderTemplate();
+	app.options = [];
+	console.log(app.options);
+	renderTemplate();
 };
 
 const onRemoveLast = () => {
-  app.options.pop();
-  console.log(app.options);
-  renderTemplate();
-}
+	app.options.pop();
+	console.log(app.options);
+	renderTemplate();
+};
 
 const onMakeDecision = () => {
-  const randNum = Math.floor(Math.random() * app.options.length);
-  const option = app.options[randNum];
-  alert(option)
-}
+	const randNum = Math.floor(Math.random() * app.options.length);
+	const option = app.options[randNum];
+	alert(option);
+};
 
-const appRoot = document.getElementById("app");
+const appRoot = document.getElementById('app');
 
 const renderTemplate = () => {
-  const template = (
-  <div>
-    <h1> {app.title} </h1>
-    {app.subTitle ? <p> {app.subTitle} </p> : false}
-    {app.options.length ? <p>Here are your options</p> : <p>No Options</p>}
-    <button disabled={app.options.length === 0} onClick={onMakeDecision}>What Should I Do</button>
-    {app.options.length ? (
-      <ol>
-        {app.options.map((option) => <li key={option}>Option: {option}</li>)}
-      </ol> ) : false
-  }
-  <button onClick={onRemoveLast}>Remove Last</button>
-  <button onClick={onRemoveAll}>Remove All</button>
-  <form onSubmit={onFormSubmit}>
-    <input type="text" name="option"/>
-    <button>Add Option</button>
-  </form>
-  </div>
-);
-ReactDOM.render(template, appRoot);
-}
+	const template = (
+		<div>
+			<h1> {app.title} </h1>
+			{app.subTitle ? <p> {app.subTitle} </p> : false}
+			{app.options.length ? <p>Here are your options</p> : <p>No Options</p>}
+			<button disabled={app.options.length === 0} onClick={onMakeDecision}>
+				What Should I Do
+			</button>
+			{app.options.length ? (
+				<ol>
+					{app.options.map(option => (
+						<li key={option}>Option: {option}</li>
+					))}
+				</ol>
+			) : (
+				false
+			)}
+			<button onClick={onRemoveLast}>Remove Last</button>
+			<button onClick={onRemoveAll}>Remove All</button>
+			<form onSubmit={onFormSubmit}>
+				<input type="text" name="option" />
+				<button>Add Option</button>
+			</form>
+		</div>
+	);
+	ReactDOM.render(template, appRoot);
+};
 
 renderTemplate();
